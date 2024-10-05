40d3480c20787c0acb60badffc1d899d1e88e1e8
Emr <127445447+EmrD@users.noreply.github.com>
2024-10-05 16:08:40 +0300

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,14 @@
+# .idx/dev.nix
+{pkgs}: {
+  ...
+  idx = {
+    workspace = {
+      onCreate = {
+        default.openFiles = [
+          "src/App.jsx"
+        ];
+        installDependencies = "npm i";
+      };
+    previews = { ... };
+  };
+}
